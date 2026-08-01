7390178ffe521c58473794931d7b7da2614d0050
Aman Sagar <amansagar102005@gmail.com>
2026-08-01 11:30:14 +0530

    Streaming Feature

--- Frontend.py
+++ Frontend.py
@@ -19,11 +19,14 @@ for message in st.session_state['message_history']:
 user_input=st.chat_input("Type Here")
 
 if user_input:
-    st.session_state['message_history'].append({'role':'user','content':user_input})
-    with st.chat_message('user'):
-        st.text(user_input)
-    response=chatbot.invoke({'messages': [HumanMessage(content=user_input)]},config=CONFIG)
-    ai_message=response['messages'][-1].text
-    st.session_state['message_history'].append({'role':'assistant','content':ai_message})
-    with st.chat_message('assistant'):
-            st.text(ai_message)
+     st.session_state['message_history'].append({'role':'user','content':user_input})
+     with st.chat_message('user'):
+          st.text(user_input)
+    
+     with st.chat_message('assistant'):
+          ai_message=st.write_stream(message_chunk.text for message_chunk,metadata in chatbot.stream(
+               {'messages': [HumanMessage(content=user_input)]},
+               config=CONFIG,
+               stream_mode='messages')
+          )
+     st.session_state['message_history'].append({'role':'assistant','content':ai_message})
