7cc2c9c58fa897b4aa3fe225443d2770f65c883b
Aman Sagar <amansagar102005@gmail.com>
2026-08-08 10:24:00 +0530

    added database

--- Backend.py
+++ Backend.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 from langchain_core.messages import BaseMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.graph.message import add_messages
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
 
 load_dotenv()
 llm=ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite")
@@ -17,8 +18,8 @@ def chat_node(state:ChatbotState):
     messages = state['messages']
     response = llm.invoke(messages)
     return {'messages': [response]}
-
-checkpoint=InMemorySaver()
+conn=sqlite3.connect(database='chatbot.db',check_same_thread=False)
+checkpointer=SqliteSaver(conn=conn)
 
 graph=StateGraph(ChatbotState)
 
@@ -26,4 +27,11 @@ graph.add_node('chat_node',chat_node)
 graph.add_edge(START,'chat_node')
 graph.add_edge('chat_node',END)
 
-chatbot = graph.compile(checkpointer=checkpoint)
+chatbot = graph.compile(checkpointer=checkpointer)
+
+
+def retrieve_all_threads():
+    all_threads=set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config["configurable"]["thread_id"])
+    return list(all_threads)    
